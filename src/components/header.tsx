1fd55a950fe8e552493066397bc307f50849ee6d
kazzyfrog <70629747+kazzyfrog@users.noreply.github.com>
2025-09-22 20:06:52 +0900

    :bug: fix: ヘッダータイトルのリンクを削除

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -6,14 +6,14 @@ export default function Header() {
     <header className="sticky top-0 z-50 container mx-auto w-full">
       <div className="container flex h-20 items-center justify-between">
         <div className="flex items-center gap-6 md:gap-10">
-          <a href="/" className="flex items-center space-x-4">
+          <span className="flex items-center space-x-4">
             <span className="font-bold text-xl md:text-2xl tracking-tight flex items-center gap-2">
               <div className="w-8 h-8 bg-indigo-600 rounded-full flex items-center justify-center">
                 <Hash className="w-5 h-5 text-indigo-50" />
               </div>
               b13oもくもくLIVE 🫐🌿🎧
             </span>
-          </a>
+          </span>
         </div>
         <div className="flex items-center gap-8">
           <a
